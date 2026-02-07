2ac59ea0a425e87a2d154c5d9395ba30e8069cf7
Armanalam7790 <alamarman7790@gmail.com>
2026-02-07 15:58:52 +0530

    oprators

---
+++ day-2-oprators/app.js
@@ -0,0 +1,28 @@
+// console.log(Math.floor(10/3)) //
+
+// console.log(2/10);
+// console.log(5678/1000);
+
+
+
+//relational oprator
+
+// console.log(10>=9)
+// console.log(10<=9);
+// console.log("9"=== 9);
+
+// console.log( 9 != 10)
+
+//logical oprator
+
+// && || !
+
+// console.log(12>7 && 8<56);
+// console.log(12>7 && 8<56 && 19<10 );
+
+
+//unary oprator
+
+
+
+
