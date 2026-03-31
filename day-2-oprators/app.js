f7b385fdf3257e498e58450b964bec9d9338d5cc
Armanalam7790 <alamarman7790@gmail.com>
2026-03-31 14:31:18 +0530

    solve question

--- day-2-oprators/app.js
+++ day-2-oprators/app.js
@@ -23,6 +23,50 @@
 
 //unary oprator
 
+//Arithmetic - + 
+//Relational Oprator
+//Logical Opretor
+//Unary Oprator
 
 
+// console.log(Math.floor(16/3));
+
+// console.log(2%10);
+
+// let num =  123456
+// // console.log(Math.floor(num/10));
+// console.log(num%100);
+
+
+
+// console.log(10>9>0);
+
+
+
+
+// console.log(Math.floor(25/4))
+// console.log(7 % 3)
+// console.log(100 % 10)
+// console.log(5 > 3)
+
+// console.log(10 > 5 > 1)
+
+// console.log(10 < 5 < 1)
+
+// console.log(true == 1)
+
+// console.log(false == 0)
+// console.log(10 > 5 && 8 < 20)
+
+// console.log(10 > 5 && 8 > 20)
+
+// console.log(!(10 > 5))
+// console.log(false + 5)
+
+// console.log("5" * "2")
+
+// console.log([] == false)
+
+// console.log([] === false)
+// console.log(null == undefined)
 
